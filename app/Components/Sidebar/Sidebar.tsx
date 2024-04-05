5c58cde49d96f064ee70afe412d1a26a51142921
rfcfa-iscte <78759743+rfcfa-iscte@users.noreply.github.com>
2024-04-05 15:52:00 +0100

    Start

---
+++ app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react'
+import styled from 'styled-components';
+
+function Sidebar() {
+  return (
+    <SidebarStyled>Sidebar</SidebarStyled>
+  )
+}
+
+const SidebarStyled = styled.nav`
+`;
+
+export default Sidebar
